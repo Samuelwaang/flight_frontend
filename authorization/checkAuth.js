fc57c7d759f1d26ee85a0c32c7d9cb2c2364ba87
Xianghui Xiao <samuelwaang@gmail.com>
2024-03-28 17:06:07 -0500

    test

--- authorization/checkAuth.js
+++ authorization/checkAuth.js
@@ -1,11 +1,11 @@
-const jwt = "";
-const cookies = document.cookie.split(';');
-for (let cookie of cookies) {
-    const [name, value] = cookie.trim().split('=');
-    if (name === 'jwt') {
-        jwt = value;
-    }
-}
+const jwt = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNzExNjYzMjA1LCJleHAiOjE3MTE2NjMyNzV9.5paeuRV9NKcWMNRb1wdacUtc4Pwzw-up3xJo_guEpSIGgYzN6cY9YnFBLMMEVWCUhpmCUR4-U8Bi1HU7KkOZxg";
+// const cookies = document.cookie.split(';');
+// for (let cookie of cookies) {
+//     const [name, value] = cookie.trim().split('=');
+//     if (name === 'jwt') {
+//         jwt = value;
+//     }
+// }
 
 fetch('http://localhost:8080/api/auth/login/check-auth', {
     method: 'GET',
